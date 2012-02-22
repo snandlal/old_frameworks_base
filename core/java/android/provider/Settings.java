ac1d281c8b4b28a90d8f1a65bebd5b90f42569fa
Davor Bertovic <davor@losinj.com>
2012-02-22 20:43:38 +0100

    Add national data roaming and make it optional (1/2)
    by Robin Morawetz (Rdlgrmpf) and Davor Bertovic (LorD ClockaN)
    
    Change-Id: Ief840d818ecf4b8ebb088fab34f01878d0a9f2d7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2108,6 +2108,11 @@ public final class Settings {
         public static final String EXPANDED_FLASH_MODE = "expanded_flash_mode";
 
         /**
+         * Whether national data Roming should be used.
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
