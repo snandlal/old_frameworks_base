55a7b007453d78e96eefcdd32042b3e81388f90d
iceandfire <arhamjamal@gmail.com>
2012-09-08 14:56:40 +0530

    Long press back key to kill the app
    Change-Id: I97c95fd269caae51b35a30830059a285f94a92cd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2015,6 +2015,13 @@ public final class Settings {
 	*/
         public static final String TORCH_STATE = "torch_state";
 
+	/**
+         * whether to enable end app on back longpress functionality
+         *
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
         /**
          * Pointer speed setting.
          * This is an integer value in a range between -7 and +7, so there are 15 possible values.
